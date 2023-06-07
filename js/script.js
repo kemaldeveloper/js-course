181227ee8c41031de7729543b02ab19c813b4ae5
Kemal <gurbansahadowkemal01@gmai.com>
2023-06-07 20:46:50 +0300

    Массивы и псевдомассивы

--- js/script.js
+++ js/script.js
@@ -1,41 +1,35 @@
 "use strict";
+const arr = [2, 13, 26, 8, 10];
 
-const options = {
-  name: 'test',
-  width: 1024,
-  height: 1024,
-  colors: {
-    border: 'black',
-    bg: 'red'
-  },
-  makeTest: function() {
-    console.log("Test");
-  }
-};
+arr.sort(compareNum);
+console.log(arr);
 
-options.makeTest();
+function compareNum(a, b) {
+  return a - b;
+}
 
-// Деструктуризация
-const {border, bg} = options.colors;
+arr[99] = 0;
+console.log(arr.length);
+console.log(arr);
 
-console.log(border);
-console.log(bg);
+arr.forEach(function(item, i, arr) {
+  console.log(`${i}: ${item} Внутри массива ${arr}`);
+});
 
-// console.log(Object.keys(options).length);
-// console.log(Object.assign(options));
+arr.pop();
+arr.push(10);
 
-// let counter = 0;
+console.log(arr);
 
-// for (let key in options) {
-//   if (typeof(options[key]) === 'object') {
-//     for (let i in options[key]) {
-//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//       counter++;
-//     }
-//   } else {
-//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//     counter++;
-//   }
-// }
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
 
-// console.log(counter);
+for (let value of arr) {
+  console.log(value);
+}
+
+const str = prompt("", "");
+const products = str.split(", ");
+products.sort();
+console.log(products.join('; '));
